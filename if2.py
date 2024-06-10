484ae9cafa8cff26ca36f2c8d1528c7659821306
itc-s24005 <s24005@std.it-college.ac.jp>
2024-06-10 15:29:51 +0900

    add if_kadai.py

---
+++ if2.py
@@ -0,0 +1,8 @@
+# s24005
+# 条件式の練習
+score = 60
+if score >= 80:
+    print("やったね！")
+    print("次もこの調子だ")
+else:
+    print("残念でした")
---
+++ if_kadai.py
@@ -0,0 +1,11 @@
+# s24005
+# scoreが60以上なら「可」、70以上なら「良」、80以上なら「優」、60未満なら「不可」と表示
+score = 23
+if score >= 80:
+    print("優")
+elif score >= 70:
+    print("良")
+elif score >= 60:
+    print("可")
+else:
+    print("不可")
